9fc169b8c1357ab4c69f70b7ab214a32f6546512
Viki Gaikwad <vikigaikwad108@gmail.com>
2025-10-15 10:07:40 +0530

    variable

---
+++ variable.py
@@ -0,0 +1,27 @@
+Name ="viki"
+age = 21
+price = 99.9
+is_student = True
+
+print("Name:",Name)
+print("age:",age)
+print("price:",price)
+print("is_student:",is_student)
+
+#checking data type
+
+print(type(Name))
+print(type(age))
+print(type(price))
+print(type(is_student))
+
+
+#Type casting
+x = 5
+y = float(x)
+print(y)
+
+#user input 
+a= input("enter your name:")
+b= int(input("enter your age:"))
+print(f"hello {a} you are {b} old")
