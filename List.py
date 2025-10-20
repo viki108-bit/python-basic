7d0f43177d844522a8721d168aa80e4a5f4b756f
Viki Gaikwad <vikigaikwad108@gmail.com>
2025-10-20 19:35:55 +0530

    list

---
+++ List.py
@@ -0,0 +1,41 @@
+# 1. List Example
+fruits = ["apple", "banana", "cherry", "apple"]
+print("Original List:", fruits)
+
+# 2. Accessing
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+
+# 3. Changing
+fruits[1] = "mango"
+print("After Change:", fruits)
+
+# 4. Adding
+fruits.append("grape")
+fruits.extend(["kiwi", "orange"])
+print("After Adding:", fruits)
+
+# 5. Removing
+fruits.remove("apple")
+fruits.pop()
+print("After Removing:", fruits)
+
+# 6. Looping
+print("Fruits List:")
+for fruit in fruits:
+    print(fruit)
+
+# 7. Slicing
+numbers = [10, 20, 30, 40, 50, 60]
+print("Sliced List:", numbers[1:4])
+
+# 8. Tuple Example
+colors = ("red", "green", "blue")
+print("Tuple:", colors)
+print("First Color:", colors[0])
+
+# 9. Tuple to List Conversion
+temp = list(colors)
+temp.append("yellow")
+colors = tuple(temp)
+print("Modified Tuple:", colors)
